e49030c47e7ec7394c96beb5176e7b20a996b984
bridgetlane <bridgetlane.mail@gmail.com>
2014-03-01 23:10:17 -0500

    Update test.js

--- test.js
+++ test.js
@@ -29,7 +29,7 @@ simply.on('singleClick', function(e) {
           
           //savedPos = data.main.temp;
           
-          });*/
+          });
           
         });
         simply.vibe('short');
