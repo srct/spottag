3d7a6868eddb3fe8c628d1c3753455b3ad4f2ae4
TheBacon <bbacon@gmu.edu>
2014-03-02 00:43:07 -0500

    Update test.js

--- test.js
+++ test.js
@@ -3,8 +3,8 @@ simply.title("\n        Spot\n        Tag");
 var savedLat = null;
 var savedLon = null;
 
-simply.style = function(large) {
-        return simply.impl.style.apply('large',Spot);
+simply.style = function('large') {
+        return simply.impl.style.apply(this,text);
 }
 simply.on('singleClick', function(e) {
     
