48e2d834834edf0027b290a40f5070d2a17f4f07
TheBacon <bbacon@gmu.edu>
2014-03-01 20:51:52 -0500

    Update test.js

--- test.js
+++ test.js
@@ -5,3 +5,12 @@ simply.on('singleClick', function(e) {
 });
 
 // I'm going to comment to organize code
+//I have commented this out until I put in a menu 
+//navigator.geolocation.getCurrentPosition(function(pos) {
+  //var coords = pos.coords;
+  //var weatherUrl = 'http://api.openweathermap.org/data/2.5/weather?' +
+  //    'lat=' + coords.latitude + '&lon=' + coords.longitude + '&units=metric';
+  //ajax({ url: weatherUrl, type: 'json' }, function(data) {
+    //simply.text({ title: data.name, subtitle: data.main.temp });
+  //});
+//});
