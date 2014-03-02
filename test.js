afbd71850f4f3d209096d5de4a46fefbafa6b72b
bridgetlane <bridgetlane.mail@gmail.com>
2014-03-01 20:44:18 -0500

    Update test.js
    Retesting original debugged code

--- test.js
+++ test.js
@@ -1,4 +1,5 @@
-ajax({ url: 'http://simplyjs.io' }, function(data){
- var headline = data.match(/<h1>(.*?)<\/h1>/)[1];
- simply.title(headline);
-}); 
+simply.title("SpotTag!");
+
+simply.on('singleClick', function(e) {
+    simply.subtitle("You pressed the " + e.button + " button!");
+});
