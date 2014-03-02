387e8073a8a5526b07703da5a387ee707c5b4270
bridgetlane <bridgetlane.mail@gmail.com>
2014-03-01 21:06:40 -0500

    Update test.js
    Adding functionality

--- test.js
+++ test.js
@@ -1,16 +1,29 @@
 simply.title("SpotTag!");
 
 simply.on('singleClick', function(e) {
+    
+    var currentPos = 0;
 
     if (e.button === "up") {
-        simply.subtitle("Track GPS here...");
+        
+        navigator.geolocation.getCurrentPosition(function(pos) {
+          var coords = pos.coords;
+          var weatherUrl = 'http://api.openweathermap.org/data/2.5/weather?' +
+           'lat=' + coords.latitude + '&lon=' + coords.longitude + '&units=metric';
+          ajax({ url: weatherUrl, type: 'json' }, function(data) {
+            simply.text("Current position marked: " { title: data.name, subtitle: data.main.temp });
+          });
+          
+
+          
+        });
     } 
     if (e.button === "down") {
         simply.subtitle("Show relative GPS here...");
     }
     
 });
-
+/*
 navigator.geolocation.getCurrentPosition(function(pos) {
   var coords = pos.coords;
   var weatherUrl = 'http://api.openweathermap.org/data/2.5/weather?' +
@@ -18,15 +31,4 @@ navigator.geolocation.getCurrentPosition(function(pos) {
   ajax({ url: weatherUrl, type: 'json' }, function(data) {
     simply.text({ title: data.name, subtitle: data.main.temp });
   });
-});
-
-// I'm going to comment to organize code
-//I have commented this out until I put in a menu 
-//navigator.geolocation.getCurrentPosition(function(pos) {
-  //var coords = pos.coords;
-  //var weatherUrl = 'http://api.openweathermap.org/data/2.5/weather?' +
-  //    'lat=' + coords.latitude + '&lon=' + coords.longitude + '&units=metric';
-  //ajax({ url: weatherUrl, type: 'json' }, function(data) {
-    //simply.text({ title: data.name, subtitle: data.main.temp });
-  //});
-//});
+}); */
