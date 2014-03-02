bd8e471a53933fa4d3864a73afdc67b8c5d64974
bridgetlane <bridgetlane.mail@gmail.com>
2014-03-01 21:10:02 -0500

    Update test.js

--- test.js
+++ test.js
@@ -12,7 +12,7 @@ simply.on('singleClick', function(e) {
           var weatherUrl = 'http://api.openweathermap.org/data/2.5/weather?' +
            'lat=' + coords.latitude + '&lon=' + coords.longitude + '&units=metric';
           ajax({ url: weatherUrl, type: 'json' }, function(data) {
-            simply.text("Current position marked: " { title: data.name, subtitle: data.main.temp });
+            simply.text({ title: data.name, subtitle: data.main.temp });
           });
           
 
@@ -25,12 +25,3 @@ simply.on('singleClick', function(e) {
     }
     
 });
-/*
-navigator.geolocation.getCurrentPosition(function(pos) {
-  var coords = pos.coords;
-  var weatherUrl = 'http://api.openweathermap.org/data/2.5/weather?' +
-   'lat=' + coords.latitude + '&lon=' + coords.longitude + '&units=metric';
-  ajax({ url: weatherUrl, type: 'json' }, function(data) {
-    simply.text({ title: data.name, subtitle: data.main.temp });
-  });
-}); */
