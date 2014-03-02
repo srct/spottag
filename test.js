cca56d1378b06f0dbdc59581580291e0f043373a
bridgetlane <bridgetlane.mail@gmail.com>
2014-03-01 23:09:36 -0500

    Update test.js

--- test.js
+++ test.js
@@ -6,7 +6,7 @@ var savedPos = 0;
     text_layer_set_text_alignment(GTextAlignmentLeft);
     text_layer_set_overflow_mode(GTextOverflowModeWordWrap);
     
-    }*/ //
+    }*/
 simply.on('singleClick', function(e) {
     
     var makeTag = "Tag Made";
@@ -19,14 +19,13 @@ simply.on('singleClick', function(e) {
           var weatherUrl = 'http://api.openweathermap.org/data/2.5/weather?' +
            'lat=' + coords.latitude + '&lon=' + coords.longitude + '&units=metric';
            
-           var sub = data.name + ":\n" + coords.latitude + "\n" + coords.longitude;
-           simply.text({ title: makeTag, subtitle: sub });
-           
-           /*
           ajax({ url: weatherUrl, type: 'json' }, function(data) {
+            var sub = data.name + ":\n" + coords.latitude + "\n" + coords.longitude;
+            simply.text({ title: makeTag, subtitle: sub });
+            /*
             simply.subtitle(coords.latitude + " " + coords.longitude);
             var sub = data.name + ":\n" + data.main.temp;
-            simply.text({ title: makeTag, subtitle: sub });
+            simply.text({ title: makeTag, subtitle: sub });*/
           
           //savedPos = data.main.temp;
           
