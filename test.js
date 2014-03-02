9bdb6ed7ab3de8f61afdae900f040c6c443cd79e
bridgetlane <bridgetlane.mail@gmail.com>
2014-03-01 23:17:53 -0500

    Update test.js

--- test.js
+++ test.js
@@ -1,6 +1,7 @@
 simply.title("\n        Spot\n        Tag");
 
-var savedPos = 0;
+var savedLat = null;
+var savedLon = null;
 /*TextLayer* text_layer_create(GRect frame){
     text_layer_set_font(fonts_get_system_font(FONT_KEY_GOTHIC_BOLD_28));
     text_layer_set_text_alignment(GTextAlignmentLeft);
@@ -22,12 +23,6 @@ simply.on('singleClick', function(e) {
           ajax({ url: weatherUrl, type: 'json' }, function(data) {
             var sub = data.name + ":\n" + coords.latitude + "\n" + coords.longitude;
             simply.text({ title: makeTag, subtitle: sub });
-            /*
-            simply.subtitle(coords.latitude + " " + coords.longitude);
-            var sub = data.name + ":\n" + data.main.temp;
-            simply.text({ title: makeTag, subtitle: sub });*/
-          
-          //savedPos = data.main.temp;
           
           });
           
@@ -35,21 +30,20 @@ simply.on('singleClick', function(e) {
         simply.vibe('short');
     }
    // text_layer_destroy(text_layer);
-    /*
     if (e.button === "down") {
         
         navigator.geolocation.getCurrentPosition(function(pos) {
           var coords = pos.coords;
           var weatherUrl = 'http://api.openweathermap.org/data/2.5/weather?' +
            'lat=' + coords.latitude + '&lon=' + coords.longitude + '&units=metric';
+          
           ajax({ url: weatherUrl, type: 'json' }, function(data) {
-            var sub = "Last: \n" + savedPos + "\n" + "Current: \n" + data.main.temp;
-            
+            var sub = "Last: \n" + savedLat + "\n" + savedLon + "\n" + "Current: \n" + coords.latitude + "\n" + coords.longitude;
             simply.text({title: trackTag, subtitle: sub})
           
           });
           
         });
     }
-    */
+
 });
