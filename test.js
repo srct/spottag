fac6215672b6c2daccbd20d3c70b5d4060178165
bridgetlane <bridgetlane.mail@gmail.com>
2014-03-02 00:59:42 -0500

    Update test.js

--- test.js
+++ test.js
@@ -2,6 +2,8 @@ simply.title("\n         Spot\n         Tag");
 
 var savedLat = null;
 var savedLon = null;
+var savedLat2 = null;
+var savedLon2 = null;
 /*
 simply.style = function('large') {
         return simply.impl.style.apply(this,text);
@@ -21,7 +23,9 @@ simply.on('singleClick', function(e) {
            
           ajax({ url: weatherUrl, type: 'json' }, function(data) {
             savedLat = coords.latitude.toFixed(4);
-            savedLon = coords.longitude.toFixed(4); 
+            savedLon = coords.longitude.toFixed(4);
+            savedLat2 = coords.latitude.toFixed(2);
+            savedLon2 = coords.longitude.toFixed(2);
             var sub = data.name + ":\nLat: " + savedLat + "\nLon: " + savedLon;
             simply.text({ title: makeTag, subtitle: sub });
           
@@ -41,7 +45,7 @@ simply.on('singleClick', function(e) {
            'lat=' + coords.latitude + '&lon=' + coords.longitude + '&units=metric';
           
           ajax({ url: weatherUrl, type: 'json' }, function(data) {
-            var sub = "Last: \n: " + savedLat.toFixed(2) + ", " + savedLon.toFixed(2) + "\n" + "Current: \n " + coords.latitude.toFixed(2) + ", " + coords.longitude.toFixed(2);
+            var sub = "Last: \n: " + savedLat2 + ", " + savedLon2 + "\n" + "Current: \n " + coords.latitude.toFixed(2) + ", " + coords.longitude.toFixed(2);
             simply.text({title: trackTag, subtitle: sub})
           
           });
