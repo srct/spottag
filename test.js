87c1347f5ed7b9618e50fefd7fa3aefae8d66d37
bridgetlane <bridgetlane.mail@gmail.com>
2014-03-01 21:18:51 -0500

    Update test.js
    Global position variable

--- test.js
+++ test.js
@@ -2,28 +2,27 @@ simply.title("SpotTag!");
 
 simply.on('singleClick', function(e) {
     
-    var currentPos = 0;
+    var savedPos = 0;
     var n1 = "title";
 
     if (e.button === "up") {
         
         navigator.geolocation.getCurrentPosition(function(pos) {
           var coords = pos.coords;
-          console.log(coords);
           var weatherUrl = 'http://api.openweathermap.org/data/2.5/weather?' +
            'lat=' + coords.latitude + '&lon=' + coords.longitude + '&units=metric';
           ajax({ url: weatherUrl, type: 'json' }, function(data) {
             simply.text({ title: data.name, subtitle: data.main.temp });
+            
+          savedPos = data.main.temp;
           });
-          
 
           
         });
         simply.vibe('short');
     } 
     if (e.button === "down") {
-        simply.text({title: n1})
-        simply.subtitle("Show relative GPS here...");
+        simply.text({title: n1, subtitle: savedPos;});
     }
     
 });
