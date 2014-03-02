bf798adfaaae499ddd7d373e6f03987674070d37
TheBacon <bbacon@gmu.edu>
2014-03-01 23:35:16 -0500

    Update test.js

--- test.js
+++ test.js
@@ -22,8 +22,8 @@ simply.on('singleClick', function(e) {
            'lat=' + coords.latitude + '&lon=' + coords.longitude + '&units=metric';
            
           ajax({ url: weatherUrl, type: 'json' }, function(data) {
-            savedLat = coords.latitude;
-            savedLon = coords.longitude;            
+            var savedLat = coords.latitude;
+            var savedLon = coords.longitude;            
             var sub = data.name + ":\n" + savedLat(0,6) + "\n" + savedLong(0,6);
             simply.text({ title: makeTag, subtitle: sub });
           
