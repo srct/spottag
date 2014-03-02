740dbcd49b369dfdcf5d18eb487fc38d3e74b087
bridgetlane <bridgetlane.mail@gmail.com>
2014-03-02 00:57:36 -0500

    Update test.js

--- test.js
+++ test.js
@@ -41,7 +41,7 @@ simply.on('singleClick', function(e) {
            'lat=' + coords.latitude + '&lon=' + coords.longitude + '&units=metric';
           
           ajax({ url: weatherUrl, type: 'json' }, function(data) {
-            var sub = "Last: \n: " + savedLat + ", " + savedLon + "\n" + "Current: \n " + coords.latitude.toFixed(4) + ", " + coords.longitude.toFixed(4);
+            var sub = "Last: \n: " + savedLat.toFixed(2) + ", " + savedLon.toFixed(2) + "\n" + "Current: \n " + coords.latitude.toFixed(2) + ", " + coords.longitude.toFixed(2);
             simply.text({title: trackTag, subtitle: sub})
           
           });
